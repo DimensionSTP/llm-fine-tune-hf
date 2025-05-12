6b08a0076832d779bc553622893d845decdfd2dd
DimensionSTP <ddang8jh@gmail.com>
2025-05-12 18:24:33 +0900

    refactor: sft dataset module init

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -1,11 +1,11 @@
-from .cpt_dataset import StructuralDataset as CPTStructuralDataset
-from .cpt_dataset import ConversationalDataset as CPTConversationalDataset
+from .sft_dataset import StructuralDataset as SFTStructuralDataset
+from .sft_dataset import ConversationalDataset as SFTConversationalDataset
 from .dpo_dataset import StructuralDataset as DPOStructuralDataset
 from .dpo_dataset import ConversationalDataset as DPOConversationalDataset
 
 __all__ = [
-    "CPTStructuralDataset",
-    "CPTConversationalDataset",
+    "SFTStructuralDataset",
+    "SFTConversationalDataset",
     "DPOStructuralDataset",
     "DPOConversationalDataset",
 ]
