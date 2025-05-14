d0a08301c322ead795a99e3ac8a9354b394d6e61
DimensionSTP <ddang8jh@gmail.com>
2025-05-15 02:31:17 +0900

    add: wus_scaling_method, wus_attention_scaling args

--- scripts/wus.sh
+++ scripts/wus.sh
@@ -5,6 +5,8 @@ upload_user="Qwen"
 model_type="Qwen3-8B"
 precision="bf16"
 wus_hidden_scale=2
+wus_scaling_method="interleaving"
+wus_attention_scaling="heads"
 num_safetensors=5
 
 python $path/wus.py \
@@ -12,4 +14,6 @@ python $path/wus.py \
     model_type=$model_type \
     precision=$precision \
     wus_hidden_scale=$wus_hidden_scale \
+    wus_scaling_method=$wus_scaling_method \
+    wus_attention_scaling=$wus_attention_scaling \
     num_safetensors=$num_safetensors
