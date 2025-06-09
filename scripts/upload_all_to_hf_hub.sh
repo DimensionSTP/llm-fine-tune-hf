af2b1801869ffff93b3a31f8d6631785ce20676f
DimensionSTP <ddang8jh@gmail.com>
2025-06-09 14:25:41 +0900

    feat: sync with default config, remain necessaries

--- scripts/upload_all_to_hf_hub.sh
+++ scripts/upload_all_to_hf_hub.sh
@@ -2,13 +2,10 @@
 
 path="src/postprocessing"
 dataset_name="tulu"
-is_sft=False
-is_preprocessed=False
 strategy="deepspeed"
-upload_user="Qwen"
-model_type="Qwen3-8B"
 model_detail="Qwen3-8B"
 upload_tag="sft"
+is_sft=True
 is_quantized=False
 is_peft=False
 max_length=4096
@@ -17,13 +14,10 @@ gradient_accumulation_steps=1
 
 python $path/upload_all_to_hf_hub.py \
     dataset_name=$dataset_name \
-    is_sft=$is_sft \
-    is_preprocessed=$is_preprocessed \
     strategy=$strategy \
-    upload_user=$upload_user \
-    model_type=$model_type \
     model_detail=$model_detail \
     upload_tag=$upload_tag \
+    is_sft=$is_sft \
     is_quantized=$is_quantized \
     is_peft=$is_peft \
     max_length=$max_length \
