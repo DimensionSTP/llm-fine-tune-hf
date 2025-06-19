cf04e791eb6637fdccad11c9e4dc05a25c012520
DimensionSTP <ddang8jh@gmail.com>
2025-06-19 14:40:28 +0900

    add: test_vllm func init

--- src/pipelines/__init__.py
+++ src/pipelines/__init__.py
@@ -1,7 +1,8 @@
-from .pipeline import train, test, test_large
+from .pipeline import train, test, test_large, test_vllm
 
 __all__ = [
     "train",
     "test",
     "test_large",
+    "test_vllm",
 ]
