886adf28f952def57a47792681c4c786371be565
DimensionSTP <ddang8jh@gmail.com>
2025-12-22 02:28:41 +0900

    add: GRPO datasets init

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -2,6 +2,8 @@ from .sft_dataset import StructuralDataset as SFTStructuralDataset
 from .sft_dataset import ConversationalDataset as SFTConversationalDataset
 from .dpo_dataset import StructuralDataset as DPOStructuralDataset
 from .dpo_dataset import ConversationalDataset as DPOConversationalDataset
+from .grpo_dataset import StructuralDataset as GRPOStructuralDataset
+from .grpo_dataset import ConversationalDataset as GRPOConversationalDataset
 from .test_dataset import StructuralDataset as TestStructuralDataset
 from .test_dataset import ConversationalDataset as TestConversationalDataset
 
@@ -10,6 +12,8 @@ __all__ = [
     "SFTConversationalDataset",
     "DPOStructuralDataset",
     "DPOConversationalDataset",
+    "GRPOStructuralDataset",
+    "GRPOConversationalDataset",
     "TestStructuralDataset",
     "TestConversationalDataset",
 ]
