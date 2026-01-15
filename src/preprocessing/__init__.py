d01743287922a6f2e15defa3410c0da49c036c39
DimensionSTP <ddang8jh@gmail.com>
2026-01-15 17:45:39 +0900

    add: dense_to_moe_qwen3 init

--- src/preprocessing/__init__.py
+++ src/preprocessing/__init__.py
@@ -1,9 +1,11 @@
 from .merge_model import merge_model
 from .merge_tokenizer import merge_tokenizer
 from .specialize_reasoning import specialize_reasoning
+from .dense_to_moe_qwen3 import dense_to_moe_qwen3
 
 __all__ = [
     "merge_model",
     "merge_tokenizer",
     "specialize_reasoning",
+    "dense_to_moe_qwen3",
 ]
