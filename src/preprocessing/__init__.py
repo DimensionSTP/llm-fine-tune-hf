502a2a20e3044a0baf39b393024834fa3282a5f6
DimensionSTP <ddang8jh@gmail.com>
2026-01-18 07:19:50 +0900

    add: verify modules init

--- src/preprocessing/__init__.py
+++ src/preprocessing/__init__.py
@@ -3,6 +3,8 @@ from .merge_tokenizer import merge_tokenizer
 from .specialize_reasoning import specialize_reasoning
 from .dense_to_moe_qwen3 import dense_to_moe_qwen3
 from .merge_dense_lora_to_moe import merge_dense_lora_to_moe
+from .verify_dense_to_moe_qwen3 import verify_dense_to_moe
+from .verify_lora_merge_to_moe import verify_lora_merge
 
 __all__ = [
     "merge_model",
@@ -10,4 +12,6 @@ __all__ = [
     "specialize_reasoning",
     "dense_to_moe_qwen3",
     "merge_dense_lora_to_moe",
+    "verify_dense_to_moe",
+    "verify_lora_merge",
 ]
