ea58a0b4e89d992e612a33e5ab47fb1d2f1f41d1
DimensionSTP <ddang8jh@gmail.com>
2026-02-01 14:48:52 +0900

    add: SingleKVReward init

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -11,6 +11,7 @@ from .rewards import (
     RougeReward,
     EquationReward,
     RetrievalHitReward,
+    SingleKVReward,
 )
 
 __all__ = [
@@ -26,4 +27,5 @@ __all__ = [
     "RougeReward",
     "EquationReward",
     "RetrievalHitReward",
+    "SingleKVReward",
 ]
