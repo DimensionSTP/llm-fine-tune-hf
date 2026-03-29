16926a6d516c74aefea597ecc2490bf9cb94705f
DimensionSTP <ddang8jh@gmail.com>
2026-03-30 01:50:57 +0900

    refactor: export sparse-decoder dense-to-moe api aliases

--- src/preprocessing/__init__.py
+++ src/preprocessing/__init__.py
@@ -2,14 +2,12 @@ from .merge_model import merge_model
 from .merge_tokenizer import merge_tokenizer
 from .specialize_reasoning import specialize_reasoning
 from .dense_to_moe_qwen3 import dense_to_moe as dense_to_moe_qwen3
-from .dense_to_moe_mixtral import dense_to_moe_mixtral
-from .extract_ministral3_text_backbone import extract_ministral3_text_backbone
+from .dense_to_moe_mixtral import dense_to_moe as dense_to_moe_sparse_decoder
 from .merge_dense_lora_to_moe import merge_dense_lora_to_moe
-from .verify_extract_ministral3_text_backbone import (
-    verify_extract_ministral3_text_backbone,
-)
 from .verify_dense_to_moe_qwen3 import verify_dense_to_moe as verify_dense_to_moe_qwen3
-from .verify_dense_to_moe_mixtral import verify_dense_to_moe_mixtral
+from .verify_dense_to_moe_mixtral import (
+    verify_dense_to_moe as verify_dense_to_moe_sparse_decoder,
+)
 from .verify_lora_merge_to_moe import verify_lora_merge
 
 __all__ = [
@@ -17,11 +15,9 @@ __all__ = [
     "merge_tokenizer",
     "specialize_reasoning",
     "dense_to_moe_qwen3",
-    "dense_to_moe_mixtral",
-    "extract_ministral3_text_backbone",
+    "dense_to_moe_sparse_decoder",
     "merge_dense_lora_to_moe",
-    "verify_extract_ministral3_text_backbone",
     "verify_dense_to_moe_qwen3",
-    "verify_dense_to_moe_mixtral",
+    "verify_dense_to_moe_sparse_decoder",
     "verify_lora_merge",
 ]
