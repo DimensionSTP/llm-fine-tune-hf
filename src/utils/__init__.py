7fd83995b2c91a1b941a363b921069762912fa7d
DimensionSTP <ddang8jh@gmail.com>
2026-04-07 03:37:37 +0900

    refactor: reorder and extend utils package exports

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,19 +1,5 @@
 from .setup import SetUp
 from .collate_fns import collate_fn_vlm
-from .reward_vector_store import FaissIndex
-from .reward_embedding import VllmEmbedding
-from .hydra_resolvers import register_hydra_resolvers
-from .vllm_sync import patch_qwen_packed_moe_vllm_sync, patch_sparse_decoder_moe_vllm_sync
-from .test_utils import (
-    build_test_dataloader,
-    generate_test_results,
-    save_test_results_json,
-    resolve_vllm_tp_size,
-    build_vllm,
-    build_sampling_params,
-    build_lora_request,
-    load_test_dataframe,
-)
 from .rewards import (
     RewardManager,
     ThinkFormatReward,
@@ -27,23 +13,30 @@ from .rewards import (
     SingleKVReward,
     MultiKVReward,
 )
+from .reward_vector_store import FaissIndex
+from .reward_embedding import VllmEmbedding
+from .hydra_resolvers import register_hydra_resolvers
+from .test_utils import (
+    build_test_dataloader,
+    generate_test_results,
+    save_test_results_json,
+    resolve_vllm_tp_size,
+    build_vllm,
+    build_sampling_params,
+    build_lora_request,
+    load_test_dataframe,
+)
+from .vllm_sync import patch_qwen_packed_moe_vllm_sync, patch_sparse_decoder_moe_vllm_sync
+from .async_grpo_runtime import (
+    resolve_async_runtime_state,
+    run_async_inference_server,
+    start_async_training_runtime,
+    stop_async_training_runtime,
+)
 
 __all__ = [
     "SetUp",
     "collate_fn_vlm",
-    "FaissIndex",
-    "VllmEmbedding",
-    "register_hydra_resolvers",
-    "patch_qwen_packed_moe_vllm_sync",
-    "patch_sparse_decoder_moe_vllm_sync",
-    "build_test_dataloader",
-    "generate_test_results",
-    "save_test_results_json",
-    "resolve_vllm_tp_size",
-    "build_vllm",
-    "build_sampling_params",
-    "build_lora_request",
-    "load_test_dataframe",
     "RewardManager",
     "ThinkFormatReward",
     "AnswerFormatReward",
@@ -55,4 +48,21 @@ __all__ = [
     "RetrievalnDCGReward",
     "SingleKVReward",
     "MultiKVReward",
+    "FaissIndex",
+    "VllmEmbedding",
+    "register_hydra_resolvers",
+    "build_test_dataloader",
+    "generate_test_results",
+    "save_test_results_json",
+    "resolve_vllm_tp_size",
+    "build_vllm",
+    "build_sampling_params",
+    "build_lora_request",
+    "load_test_dataframe",
+    "patch_qwen_packed_moe_vllm_sync",
+    "patch_sparse_decoder_moe_vllm_sync",
+    "resolve_async_runtime_state",
+    "run_async_inference_server",
+    "start_async_training_runtime",
+    "stop_async_training_runtime",
 ]
