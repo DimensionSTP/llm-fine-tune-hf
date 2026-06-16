e283a497a906cf379634ff4c1010727ceced6671
DimensionSTP <ddang8jh@gmail.com>
2026-06-16 21:41:02 +0900

    add: export chat template kwarg filter helper

--- src/helpers/__init__.py
+++ src/helpers/__init__.py
@@ -3,11 +3,12 @@ from .dataset_paths import (
     build_dataset_file_path_metadata,
     resolve_dataset_file_path,
 )
-from .chat_template import build_enable_thinking_kwargs
+from .chat_template import build_enable_thinking_kwargs, filter_chat_template_kwargs
 
 __all__ = [
     "build_dataset_file_name",
     "build_dataset_file_path_metadata",
     "resolve_dataset_file_path",
     "build_enable_thinking_kwargs",
+    "filter_chat_template_kwargs",
 ]
