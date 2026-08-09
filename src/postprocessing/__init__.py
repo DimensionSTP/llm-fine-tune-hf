eb991c871571649c4fc3d6fefa1a2035863802d8
DimensionSTP <ddang8jh@gmail.com>
2026-08-10 03:43:40 +0900

    refactor: export postprocessing APIs

--- src/postprocessing/__init__.py
+++ src/postprocessing/__init__.py
@@ -1,1 +1,11 @@
-__all__ = []
+from .merge_lora import merge_lora
+from .upload_to_hf_hub import upload_to_hf_hub
+from .upload_all_to_hf_hub import upload_to_hf_hub as upload_all_to_hf_hub
+from .artifacts import resolve_existing_artifact_output_dir
+
+__all__ = [
+    "merge_lora",
+    "upload_to_hf_hub",
+    "upload_all_to_hf_hub",
+    "resolve_existing_artifact_output_dir",
+]
