82cbe71e7dd917cf31cc9e743fa06e5bdde027c5
DimensionSTP <ddang8jh@gmail.com>
2026-08-18 03:11:28 +0900

    add: export distillation datasets in method order

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -2,12 +2,16 @@ from .sft_dataset import StructuralDataset as SFTStructuralDataset
 from .sft_dataset import ConversationalDataset as SFTConversationalDataset
 from .dpo_dataset import StructuralDataset as DPOStructuralDataset
 from .dpo_dataset import ConversationalDataset as DPOConversationalDataset
-from .grpo_dataset import StructuralDataset as GRPOStructuralDataset
-from .grpo_dataset import ConversationalDataset as GRPOConversationalDataset
 from .kto_dataset import StructuralDataset as KTOStructuralDataset
 from .kto_dataset import ConversationalDataset as KTOConversationalDataset
 from .gkd_dataset import StructuralDataset as GKDStructuralDataset
 from .gkd_dataset import ConversationalDataset as GKDConversationalDataset
+from .distillation_dataset import StructuralDataset as DistillationStructuralDataset
+from .distillation_dataset import (
+    ConversationalDataset as DistillationConversationalDataset,
+)
+from .grpo_dataset import StructuralDataset as GRPOStructuralDataset
+from .grpo_dataset import ConversationalDataset as GRPOConversationalDataset
 from .test_dataset import StructuralDataset as TestStructuralDataset
 from .test_dataset import ConversationalDataset as TestConversationalDataset
 from .image_augmentation import build_image_augmenter
@@ -42,12 +46,14 @@ __all__ = [
     "SFTConversationalDataset",
     "DPOStructuralDataset",
     "DPOConversationalDataset",
-    "GRPOStructuralDataset",
-    "GRPOConversationalDataset",
     "KTOStructuralDataset",
     "KTOConversationalDataset",
     "GKDStructuralDataset",
     "GKDConversationalDataset",
+    "DistillationStructuralDataset",
+    "DistillationConversationalDataset",
+    "GRPOStructuralDataset",
+    "GRPOConversationalDataset",
     "TestStructuralDataset",
     "TestConversationalDataset",
     "build_image_augmenter",
